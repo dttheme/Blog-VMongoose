92e8902b8fcc53d7855229d3873f6b3d224843de
dttheme <dttheme@gmail.com>
2018-01-25 19:15:22 -0500

    added DELETE, PUT endpoints

--- server.js
+++ server.js
@@ -66,4 +66,75 @@ app.post('/blog-posts', (req, res) => {
       });
 });
 
-app.put('/blog-posts', (req, res) =>)
+app.put('/blog-posts/:id', (req, res) => {
+    if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+      const message = (
+        `Request path id (${req.params.id}) amd request body id (${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message});
+    }
+    const toUpdate = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        toUpdate[field] = req.body[field];
+      }
+    });
+
+    BlogPost
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(blog_post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error'}));
+
+});
+
+app.delete('/blog-posts/:id', (req, res) => {
+  BlogPost
+  .findByIdAndRemove(req.params.id)
+  .then(blog_post => res.status(204).end())
+  .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+app.use('*', function(req, res) {
+  res.status(404).json({ message: 'Not Found'};)
+});
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if(err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  })
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
